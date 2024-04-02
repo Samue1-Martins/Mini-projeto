d9938f53735b05a5c2bdc00372ff377117f76cec
SamuelDouglas <samuel30douglas@gmail.com>
2024-04-02 20:55:19 -0300

    adding axios to the project

--- Frontend/src/routes/home.jsx
+++ Frontend/src/routes/home.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css"
 
 const Home = () =>{
+
+    const [posts, setPosts] = useState([])
+    
+    const getPosts = async() => {
+        
+        try {
+            const response = await axios.get("")
+            
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }   
+
+    useEffect(()=>{
+        getPosts();
+    }, [])
+
     return <div>Home</div>
 };
 
