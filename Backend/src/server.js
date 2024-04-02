af7ef7c34488fd0b72f3ca15c63321198e6d3952
SamuelDouglas <samuel30douglas@gmail.com>
2024-04-02 20:53:56 -0300

    adding cors in the backend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,12 @@
 import express from "express"
+import cors from "cors"
 import { superiorsExercise } from "./database/superiors.exercises.js";
 import { routes } from "./routes/index.routes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(routes);
 
 app.get("/teste", function (req, res){
